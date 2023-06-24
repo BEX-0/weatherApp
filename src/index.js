eac24319b813f308e09b2a5c15f8694adc57e862
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-06-23 19:25:14 -0500

    moved all functions re updating current weather data into one

--- src/index.js
+++ src/index.js
@@ -55,6 +55,63 @@ function changeFiveDay() {
 
 changeFiveDay();
 
+function formateDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let currentDateTime = `${day} ${hour}:${minute}`;
+
+  let dateTime = document.querySelector(".dateTime");
+  dateTime.innerHTML = `${currentDateTime}`;
+
+  return currentDateTime;
+}
+
+formateDate();
+
+function changeFiveDay() {
+  let fiveDay = document.querySelector(".five-day");
+  let fiveDayHTML =`<div class="row">`;
+
+  let days = ["Sun","Mon","Tues","Wed","Thu"]
+  days.forEach(function(day) {
+    fiveDayHTML = fiveDayHTML + `
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <strong>${day}</strong>
+          <br />
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"" alt="Cloudy" id="fiveDayEmoji">
+          <div class="highLow">
+          <strong>19°</strong>
+          <br />
+          10°
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  })
+
+  fiveDayHTML = fiveDayHTML + `</div>`
+  fiveDay.innerHTML = fiveDayHTML;
+}
+
+changeFiveDay();
+
 function changeCity(event) {
   event.preventDefault();
   let input = document.querySelector("#cityInput");
@@ -68,43 +125,30 @@ function changeCity(event) {
   let apiKey = "82f43b0671f2tb328187o7be4ab620aa";
   let cityName = document.querySelector("#cityInput").value;
   let url = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
-  axios.get(url).then(changeCurrentTemp);
-  axios.get(url).then(changeCondition);
-  axios.get(url).then(changeHumidity);
-  axios.get(url).then(changeWind);
-  axios.get(url).then(changeEmoji);
+  axios.get(url).then(changeCurrentWeather);
 }
 let form = document.querySelector("#citySearch");
 form.addEventListener("submit", changeCity);
 
 let currentTemperature = document.querySelector(".currentTemperature");
 
-function changeCurrentTemp(response) {
+function changeCurrentWeather(response) {
   let currentTemp = Math.round(response.data.temperature.current);
-  currentTemperature.innerHTML = `${currentTemp}`;
+  currentTemperature.innerHTML = currentTemp;
 
   celsiusTemp = response.data.temperature.current;
-}
 
-function changeCondition(response) {
-  console.log(response.data)
   let condition = document.querySelector(".currentCondition");
   condition.innerHTML = response.data.condition.description;
-}
 
-function changeHumidity(response) {
   let humidity = document.querySelector(".humidity");
   let currentHumidity = response.data.temperature.humidity;
   humidity.innerHTML = `${currentHumidity}%`;
-}
-
-function changeWind(response) {
+  
   let wind = document.querySelector(".wind");
   let currentWind = response.data.wind.speed;
   wind.innerHTML = `${currentWind}km/h`;
-}
-
-function changeEmoji(response) {
+  
   let currentEmoji = document.querySelector("#currentEmoji");
   currentEmoji.setAttribute("src" , `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
 }
@@ -115,7 +159,6 @@ function convertToFahrenheit(event) {
   fahrenheitLink.classList.add("active");
   let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
   currentTemperature.innerHTML = Math.round(fahrenheitTemp);
-
 }
 
 let celsiusTemp = null;
@@ -132,4 +175,4 @@ function convertToCelsius(event) {
 
 
 let celsiusLink = document.querySelector("#celsiusLink");
-celsiusLink.addEventListener("click", convertToCelsius);
+celsiusLink.addEventListener("click", convertToCelsius);	
